6638b7c7fb27dcd540a6dced597b2e2d76646f18
Anik Barua <anikbb05@gmail.com>
2025-05-17 21:19:56 +0600

    Add project files.

---
+++ Core_Assignment_1/DBContext/AppDBContext.cs
@@ -0,0 +1,12 @@
+﻿using Core_Assignment_1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Core_Assignment_1.DBContext
+{
+    public class AppDBContext:DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options):base(options) { }
+
+        DbSet<RegistrationClass> Registrations {  get; set; }
+    }
+}
---
+++ Core_Assignment_1/Models/RegistrationClass.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core_Assignment_1.Models
+{
+    public class RegistrationClass
+    {
+        [Key]
+        public int ID { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }   
+    }
+}
